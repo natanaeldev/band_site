b0510bfbd6a1b0c853e58c353caf18b94def807f
natanael <nathan0002@outlook.es>
2022-02-17 00:25:05 -0500

    I changed the commentsArray to be hardcoded to get the data from the api and display the comments

--- js/index-page.js
+++ js/index-page.js
@@ -1,61 +1,53 @@
-const commentsArray = [
-  {
-    name: "Connor Walton",
-    date: "02 / 17 / 2021",
-    comment:
-      "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.",
-  },
-  {
-    name: "Connor Walton",
-    date: "02 / 17 / 2021",
-    comment:
-      "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.",
-  },
-  {
-    name: "Connor Walton",
-    date: " 02 / 17 / 2021",
-    comment:
-      "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.",
-  },
-];
+const apiKey = "b755b467-27d6-4447-ad13-f5267b05306c";
+const commentsArray = [];
+
+axios
+  .get(`https://project-1-api.herokuapp.com/comments?api_key=${apiKey}`)
+  .then((comment) => {
+    commentsArray.push(comment.data);
+  })
+  .then(() => {
+    commentsArray.forEach((comment) => {
+      console.log(comment);
+      displayComment(comment);
+    });
+  });
 
 let form = document.querySelector("#conversation__form");
 let commentsWrapper = document.querySelector(".comments__wrapper");
 let currentDate = new Date().toLocaleDateString();
 
 function displayComment(comment) {
-  let comments = document.createElement("div");
-  comments.classList.add("comment");
-  let headerDiv = document.createElement("div");
-  headerDiv.classList.add("comment__header");
-  let img = document.createElement("img");
-  img.setAttribute("src", "./assets/Images/Mohan-muruge.jpg");
-  img.classList.add("avatar1");
-  let commentUl = document.createElement("ul");
-  commentUl.classList.add("comment__ul");
-  let names = document.createElement("li");
-  names.classList.add("comment__li");
-  let Dates = document.createElement("li");
-  Dates.classList.add("comment__li--date");
-  let paragraph = document.createElement("p");
-  paragraph.classList.add("comment__p");
-  commentsWrapper.classList.add("comments__wrapper");
-  commentsWrapper.prepend(comments);
-  comments.appendChild(headerDiv);
-  headerDiv.appendChild(img);
-  headerDiv.appendChild(commentUl);
-  commentUl.appendChild(names).innerText = comment.name;
-  commentUl.appendChild(Dates).innerText = comment.date;
-  comments.appendChild(paragraph).innerText = comment.comment;
+  comment.forEach((comment) => {
+    let comments = document.createElement("div");
+    comments.classList.add("comment");
+    let headerDiv = document.createElement("div");
+    headerDiv.classList.add("comment__header");
+    let img = document.createElement("img");
+    img.setAttribute("src", "./assets/Images/Mohan-muruge.jpg");
+    img.classList.add("avatar1");
+    let commentUl = document.createElement("ul");
+    commentUl.classList.add("comment__ul");
+    let names = document.createElement("li");
+    names.classList.add("comment__li");
+    let Dates = document.createElement("li");
+    Dates.classList.add("comment__li--date");
+    let paragraph = document.createElement("p");
+    paragraph.classList.add("comment__p");
+    commentsWrapper.classList.add("comments__wrapper");
+    let time = new Date(comment.timestamp);
+    commentsWrapper.prepend(comments);
+    comments.appendChild(headerDiv);
+    headerDiv.appendChild(img);
+    headerDiv.appendChild(commentUl);
+    commentUl.appendChild(names).innerText = comment.name;
+    commentUl.appendChild(Dates).innerText = time.toUTCString();
+    comments.appendChild(paragraph).innerText = comment.comment;
+  });
 }
 
-commentsArray.forEach((comment) => {
-  displayComment(comment);
-});
-
 // this is the code for the button which is going to submit the comments.
-
-form.addEventListener("submit", (e) => {
+function handleSubmit(e) {
   e.preventDefault();
   let name = e.target.name.value;
 
@@ -83,4 +75,6 @@ form.addEventListener("submit", (e) => {
   commentUl.appendChild(names).innerText = name;
   commentUl.appendChild(Dates).innerText = currentDate;
   comments.appendChild(paragraph).innerText = comment;
-});
+}
+
+form.addEventListener("submit", handleSubmit);
