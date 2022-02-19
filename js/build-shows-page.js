a168412acf4c0b104b2a4a232e23773c08a261f5
natanael <nathan0002@outlook.es>
2022-02-18 19:06:36 -0500

    I made change to the class conventions

--- js/build-shows-page.js
+++ js/build-shows-page.js
@@ -5,67 +5,73 @@ const showsDates = [];
 axios.get(`${url}${apiKey}`).then((show) => {
   showsDates.push(show.data);
   showsDates.forEach((show) => {
-    displayShows(show);
+    show.forEach((data) => {
+      displayShows(data);
+    });
   });
 });
 
 // Creating the div for the shows table
-function displayShows(show) {
-  show.forEach((show) => {
-    let shows = document.querySelector(".shows");
-    let showsWrapper = document.createElement("div");
-    showsWrapper.classList.add("shows__wrapper");
-    let showList = document.createElement("ul");
-    showList.classList.add("shows__list");
-    //creating the li for the title
-    let showItemtDate = document.createElement("li");
-    let showItemtVenue = document.createElement("li");
-    let showItemtLocation = document.createElement("li");
-    showItemtDate.classList.add("shows__item--title");
-    showItemtDate.classList.add("hidden");
-    showItemtVenue.classList.add("shows__item--title");
-    showItemtVenue.classList.add("hidden");
-    showItemtLocation.classList.add("shows__item--title");
-    showItemtLocation.classList.add("hidden");
+let displayShows = (show) => {
+  let shows = document.querySelector(".shows");
+  let showsWrapper = document.createElement("div");
+  showsWrapper.classList.add("shows__wrapper");
+  let showList = document.createElement("ul");
+  showList.classList.add("shows__list");
+  //creating the li for the title
+  let showItemtDate = document.createElement("li");
+  let showItemtVenue = document.createElement("li");
+  let showItemtLocation = document.createElement("li");
+  showItemtDate.classList.add("shows__item--title");
+  showItemtDate.classList.add("hidden");
+  showItemtVenue.classList.add("shows__item--title");
+  showItemtVenue.classList.add("hidden");
+  showItemtLocation.classList.add("shows__item--title");
+  showItemtLocation.classList.add("hidden");
 
-    //creating the li for the content
-    let showItemDate = document.createElement("li");
-    let showItemVenue = document.createElement("li");
-    let showItemLocation = document.createElement("li");
-    let showItemButton = document.createElement("li");
-    let showDisplayButton = document.createElement("button");
-    let showButton = document.createElement("button");
-    showDisplayButton.innerText = "BUY TICKETS";
-    showDisplayButton.classList.add("shows__button");
-    showDisplayButton.classList.add("phone-hidden");
+  //creating the li for the content
+  let showItemDate = document.createElement("li");
+  let showItemVenue = document.createElement("li");
+  let showItemLocation = document.createElement("li");
+  let showItemButton = document.createElement("li");
+  let showDisplayButton = document.createElement("button");
+  let showButton = document.createElement("button");
+  showDisplayButton.innerText = "BUY TICKETS";
+  showDisplayButton.classList.add("shows__button");
+  showDisplayButton.classList.add("phone-hidden");
 
-    //adding the information
-    showItemDate.classList.add("shows__item");
-    showItemVenue.classList.add("shows__item");
-    showItemLocation.classList.add("shows__item");
-    showItemButton.classList.add("shows__item");
+  //adding the information
+  showItemDate.classList.add("shows__item");
+  showItemVenue.classList.add("shows__item");
+  showItemLocation.classList.add("shows__item");
+  showItemButton.classList.add("shows__item");
 
-    showButton.innerText = "BUY TICKETS";
-    showButton.classList.add("shows__button");
+  showButton.innerText = "BUY TICKETS";
+  showButton.classList.add("shows__button");
 
-    shows.appendChild(showsWrapper);
+  shows.appendChild(showsWrapper);
 
-    //applying information to the list.
-    let date = new Date(show.date);
-    date.getTime();
-    showsWrapper.appendChild(showList);
-    showList.appendChild(showItemtDate).innerText = "DATE";
-    showList.appendChild(showItemDate).innerText = date.toString();
-    showList.appendChild(showItemtVenue).innerText = "VENUE";
-    showList.appendChild(showItemVenue).innerText = show.place;
-    showList.appendChild(showItemtLocation).innerText = "LOCATION";
-    showList.appendChild(showItemLocation).innerText = show.location;
-    showList.appendChild(showItemButton);
-    showList.appendChild(showDisplayButton);
-    showButton.classList.add("hidden");
-    showItemButton.appendChild(showButton);
-    showList.addEventListener("click", () => {
-      showList.classList.add("active");
-    });
+  //applying information to the list.
+
+  // let date = () =>{
+  //  let  date =  new Date(Number(show.date));
+
+  // }
+
+  console.log(date);
+  date.toLocaleString();
+  showsWrapper.appendChild(showList);
+  showList.appendChild(showItemtDate).innerText = "DATE";
+  showList.appendChild(showItemDate).innerText = date;
+  showList.appendChild(showItemtVenue).innerText = "VENUE";
+  showList.appendChild(showItemVenue).innerText = show.place;
+  showList.appendChild(showItemtLocation).innerText = "LOCATION";
+  showList.appendChild(showItemLocation).innerText = show.location;
+  showList.appendChild(showItemButton);
+  showList.appendChild(showDisplayButton);
+  showButton.classList.add("hidden");
+  showItemButton.appendChild(showButton);
+  showList.addEventListener("click", () => {
+    showList.classList.add("active");
   });
-}
+};
--- js/index-page.js
+++ js/index-page.js
@@ -1,21 +1,23 @@
+"use strict";
 const apiKey = "b755b467-27d6-4447-ad13-f5267b05306c";
 const url = "https://project-1-api.herokuapp.com/comments?api_key=";
+
 const commentsArray = [];
 let form = document.querySelector("#conversation__form");
 let commentsWrapper = document.querySelector(".comments__wrapper");
 let currentDate = new Date().toLocaleDateString();
 
+// This api call display all the comments
 axios.get(`${url}${apiKey}`).then((comment) => {
   commentsArray.push(comment.data);
   commentsArray.forEach((comment) => {
     comment.forEach((data) => {
-      console.log(data);
       displayComment(data);
     });
   });
 });
 
-function displayComment(comment) {
+let displayComment = (comment) => {
   console.log(comment);
   let comments = document.createElement("div");
   comments.classList.add("comment");
@@ -53,9 +55,10 @@ function displayComment(comment) {
   commentUl.appendChild(Dates).innerText = time.toUTCString();
 
   comments.appendChild(paragraph).innerText = comment.comment;
-}
+};
 
 // this is the code for the button which is going to submit the comments.
+
 let handleSubmit = (e) => {
   e.preventDefault();
   let name = e.target.name.value;
@@ -66,7 +69,7 @@ let handleSubmit = (e) => {
   };
 
   if (name && comment) {
-    // commentsWrapper.innerText = "";
+    commentsWrapper.innerText = "";
     axios
       .post(`${url}${apiKey}`, newComment)
       .then((response) => {
@@ -93,29 +96,3 @@ let handleSubmit = (e) => {
 };
 
 form.addEventListener("submit", handleSubmit);
-
-// form.addEventListener("submit", handleSubmit);
-
-// let comments = document.createElement("div");
-// comments.classList.add("comment");
-// let headerDiv = document.createElement("div");
-// headerDiv.classList.add("comment__header");
-// let img = document.createElement("img");
-// img.setAttribute("src", "./assets/Images/Mohan-muruge.jpg");
-// img.classList.add("avatar1");
-// let commentUl = document.createElement("ul");
-// commentUl.classList.add("comment__ul");
-// let names = document.createElement("li");
-// names.classList.add("comment__li");
-// let Dates = document.createElement("li");
-// Dates.classList.add("comment__li--date");
-// let paragraph = document.createElement("p");
-// paragraph.classList.add("comment__p");
-// commentsWrapper.prepend(comments);
-// commentsWrapper.classList.add("comments__wrapper");
-// comments.appendChild(headerDiv);
-// headerDiv.appendChild(img);
-// headerDiv.appendChild(commentUl);
-// commentUl.appendChild(names).innerText = name;
-// commentUl.appendChild(Dates).innerText = currentDate;
-// comments.appendChild(paragraph).innerText = comment;
